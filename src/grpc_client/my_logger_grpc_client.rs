aeb68ce76f2b74442e29a097e3adaa677c87e729
amigin <amigin@gmail.com>
2024-03-06 14:40:53 +0200

    Increased Timeout

--- src/grpc_client/my_logger_grpc_client.rs
+++ src/grpc_client/my_logger_grpc_client.rs
@@ -4,7 +4,7 @@ use my_grpc_extensions::client::*;
     proto_file: "./proto/MyLogger.proto",
     crate_ns: "crate::my_logger_grpc",
     retries: 3,
-    request_timeout_sec: 1,
+    request_timeout_sec: 10,
     ping_timeout_sec: 1,
     ping_interval_sec: 3,
 )]
