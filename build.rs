b723dcddb9c64de392b25da6ae9ddb29d23e5a0d
amigin <amigin@gmail.com>
2025-06-09 15:32:47 +0300

    Fixes

--- build.rs
+++ build.rs
@@ -1,9 +1,5 @@
 fn main() {
-    ci_utils::sync_and_build_proto_file_from_private_github_repo(
-        "my-logger-hub",
-        "my-logger-server",
-        "proto/MyLogger.proto",
-    );
-
-    ci_utils::tonic_build::compile_protos("proto/MyLogger.proto").unwrap();
+    let url =
+        "https://raw.githubusercontent.com/my-logger-hub/my-logger-server/refs/heads/main/proto/";
+    ci_utils::sync_and_build_proto_file(url, "MyLogger.proto");
 }
